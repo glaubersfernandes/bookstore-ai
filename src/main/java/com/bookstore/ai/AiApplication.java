addb7f53ea810db84ca1fc8e2d151a262f8ddaee
glauber.fernandes <glaubersfernandes@gmail.com>
2026-02-27 23:42:30 -0300

    Configurando a API KEY da OpenAI

--- src/main/java/com/bookstore/ai/AiApplication.java
+++ src/main/java/com/bookstore/ai/AiApplication.java
@@ -2,10 +2,18 @@ package com.bookstore.ai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class AiApplication {
 
+	@GetMapping("/")
+	public String status() {
+		return "Bem-vindo ao BookStoreAI";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AiApplication.class, args);
 	}
