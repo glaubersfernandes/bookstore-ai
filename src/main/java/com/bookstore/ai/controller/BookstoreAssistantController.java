b806823df7efeebc29c8b4940e06ed1958451ce8
glauber.fernandes <glaubersfernandes@gmail.com>
2026-03-21 21:19:46 -0300

    Trabalhando com Flux(reactor) e Stream

--- src/main/java/com/bookstore/ai/controller/BookstoreAssistantController.java
+++ src/main/java/com/bookstore/ai/controller/BookstoreAssistantController.java
@@ -1,12 +1,16 @@
 package com.bookstore.ai.controller;
 
 import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.model.Generation;
 import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/bookstore")
@@ -18,9 +22,40 @@ public class BookstoreAssistantController {
 		this.chatModel = chatModel;
 	}
 
+	/*
+	 * @GetMapping("/informations") public ChatResponse bookstoreChat(
+	 * 
+	 * @RequestParam(value = "message", defaultValue =
+	 * "Quais são os livros best sellers dos ultimos anos?") String message) {
+	 * return chatModel.call(new Prompt(message)); }
+	 */
+
 	@GetMapping("/informations")
-	public ChatResponse bookstoreChat(
+	public Generation bookstoreChatEx2(
 			@RequestParam(value = "message", defaultValue = "Quais são os livros best sellers dos ultimos anos?") String message) {
-		return chatModel.call(new Prompt(message));
+		return chatModel.call(new Prompt(message)).getResult();
+	}
+
+	@GetMapping("/reviews")
+	public String bookstoreReview(@RequestParam(value = "book", defaultValue = "Dom Quixote") String book) {
+		PromptTemplate promptTemplate = new PromptTemplate("""
+					Por favor, me forneça um breve resumo do livo {book}
+					e também a biografia de seu autor.
+				""");
+		promptTemplate.add("book", book);
+
+		return this.chatModel.call(promptTemplate.create()).getResult().getOutput().getText();
+	}
+	
+	/*@GetMapping("/stream/informations")
+	public Flux<String> bookstoreChatStream(
+			@RequestParam(value = "message", defaultValue = "Quais são os livros best sellers dos ultimos anos?") String message) {
+		return chatModel.stream(message);
+	}*/
+	
+	@GetMapping("/stream/informations")
+	public Flux<ChatResponse> bookstoreChatStream(
+			@RequestParam(value = "message", defaultValue = "Quais são os livros best sellers dos ultimos anos?") String message) {
+		return chatModel.stream(new Prompt(message));
 	}
 }
